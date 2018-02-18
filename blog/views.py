4787359c4961356b7d1963bafe2d3ed7ee2b7a37
deadlyhallows <deotaleawanti@gmail.com>
2018-02-18 21:41:51 +0530

    Modified

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import  get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
@@ -13,9 +13,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_new(request):
     if request.method == "POST":
